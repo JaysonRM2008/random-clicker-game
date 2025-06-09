81d0b53d335b60636f2714213ccedee280db5352
Jayson Ramirez <jaysonramirez@gmail.com>
2025-06-09 13:27:30 -0400

    added clicking

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ function changePoint(amount) {
     points = points + amount
 
     console.log(points)
-    pointCounter.innerText = points + ' Pizza Piont'
+    pointCounter.innerText = points.toFixed + ' Pizza Piont'
 }
 
 function buyRushHour() {
@@ -18,4 +18,19 @@ function buyRushHour() {
     } else {
         alert("your money sucks")
     }
+}
+
+function buyfamousPeople() {
+    if (points >= 1000) {
+        changePoint(-100)
+        RushHourLevel = RushHourLevel + 1
+    } else {
+        alert("your money sucks") 
+    }
+
+
+function bigClick() {
+    let basePoint = 1 + (buyRushHourn ** 1.05) + (buyfamouspeople ** 100.05)
+
+    changePoint(basePoint)
 }
